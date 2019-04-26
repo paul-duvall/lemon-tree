4c253aee20bad31b61b31e6d196849b558fdadac
Paul Duvall <duvallpj@gmail.com>
2019-04-26 10:01:45 +0100

    Impliment JS data structure and basic JS for menu

---
+++ menu.js
@@ -0,0 +1,37 @@
+const breakfastItems = [
+  {
+    name: 'The Big Lemon Tree Breakfast',
+    price: '8.30',
+    description: 'Two rashers of back bacon, three quality butchers chipolatas, poached egg, ramekin of beans, sautéed mushrooms, potatoes, oven-roasted tomatoes and toast.',
+    extra: 'Or 5 items for £5.50!'    
+  },
+  {
+    name: 'Bacon or Sausage Ciabata',
+    price: '5.20',
+    description: 'Served on a toasted Ciabatta roll with a side of dressed rocket leaves.',
+    extra: ''    
+  },
+  {
+    name: '',
+    price: '',
+    description: '',
+    extra: ''    
+  },
+];
+
+let breakfast = document.querySelector('.c-menu__menu-section-breakfast');
+
+breakfastItems.forEach((menuItem) => {
+  breakfast.innerHTML += `
+  <div class="c-menu__item">
+    <div class="c-menu__item-name">${menuItem.name}</div>
+    <p class="c-menu__item-price">${menuItem.price}</p>
+    <p class="c-menu__item-desc">${menuItem.description}</p>
+  </div>
+`;
+if(menuItem.extra) {
+  breakfast.innerHTML += `
+    <p class="c-menu__item-extra">${menuItem.extra}</p>
+  `;
+}
+});
