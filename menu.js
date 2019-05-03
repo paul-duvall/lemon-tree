246e5b0e10127ea80b010356667e9aa726178433
Paul Duvall <duvallpj@gmail.com>
2019-05-03 17:12:39 +0100

    Add styling for responsive menu page

--- menu.js
+++ menu.js
@@ -2,126 +2,101 @@ const breakfastItems = [
   {
     name: 'The Big Lemon Tree Breakfast',
     price: '8.30',
-    description: 'Two rashers of back bacon, three quality butchers chipolatas, poached egg, ramekin of beans, sautéed mushrooms, potatoes, oven-roasted tomatoes and toast.',
-    extra: 'Or 5 items for £5.50!'    
+    description: 'Two rashers of back bacon, three quality butchers chipolatas, poached egg, ramekin of beans, sautéed mushrooms, potatoes, oven-roasted tomatoes and toast.<br><strong>Or 5 items for £5.50!</strong>'   
   },
   {
     name: 'Bacon or Sausage Ciabata',
     price: '5.20',
-    description: 'Served on a toasted Ciabatta roll with a side of dressed rocket leaves.',
-    extra: ''    
+    description: 'Served on a toasted Ciabatta roll with a side of dressed rocket leaves.'
   },
   {
     name: 'Mushrooms On Toast (V)',
     price: '5.50',
-    description: 'Sauteed button mushrooms on toast, drizzled with olive oil and garnished with dressed rocket leaves.',
-    extra: ''    
+    description: 'Sauteed button mushrooms on toast, drizzled with olive oil and garnished with dressed rocket leaves.'
   },
   {
     name: 'Eggs Anyhow (V)',
-    price: '2 free range eggs fried, poached or scrambled and served on toast. Top with two extra bits of bacon or sausages for <strong>£2.00</strong>',
-    description: '',
-    extra: ''    
+    price: '5.70',
+    description: '2 free range eggs fried, poached or scrambled and served on toast. Top with two extra bits of bacon or sausages for <strong>£2.00</strong>'
   },
   {
     name: 'The Vegetarian LemonTree (V)',
     price: '8.20',
-    description: 'Vegetarian sausage, poached egg, 2 hash browns, sauteed mushrooms, tomato, ramekin of beans and toast.',
-    extra: ''    
+    description: 'Vegetarian sausage, poached egg, 2 hash browns, sauteed mushrooms, tomato, ramekin of beans and toast.'
   },
   {
     name: 'Eggs Benedict OR Florentine',
     price: '7.00',
-    description: 'English muffin topped with our home cooked gammon, poached eggs and hollindaise sauce, with dressed leaves.<br>Florentine - swap the gammon for wilted spinach.',
-    extra: ''    
+    description: 'English muffin topped with our home cooked gammon, poached eggs and hollindaise sauce, with dressed leaves.<br>Florentine - swap the gammon for wilted spinach.'
   },
   {
     name: 'Bacon OR Sausage Sandwich',
     price: '4.50',
-    description: '',
-    extra: 'Add an egg for 90p!'    
+    description: '<strong>Add an egg for 90p!</strong>'
   },
   {
     name: 'The Healthy LemonTree (GF)',
     price: '7.30',
-    description: '2 rashers of sliced bacon on a bed of dressed baby spinach, rocket, goats cheese, topped with a poached egg.',
-    extra: ''    
+    description: '2 rashers of sliced bacon on a bed of dressed baby spinach, rocket, goats cheese, topped with a poached egg.'
   },
   {
     name: 'Cowboy Beans',
     price: '7.00',
-    description: 'Homemade beans with bacon and chorizo served on a slice of homemade sourdough toast and a side of dressed rocket leaves.',
-    extra: ''    
+    description: 'Homemade beans with bacon and chorizo served on a slice of homemade sourdough toast and a side of dressed rocket leaves.'
   },
   {
     name: 'VeggieBoy Beans',
     price: '6.40',
-    description: 'Cowboy beans but without the meaty bits.',
-    extra: ''    
-  },
-  {
-    name: '',
-    price: '',
-    description: '',
-    extra: ''    
-  },
+    description: 'Cowboy beans but without the meaty bits.'
+  }
 ];
 
 const mainsItems = [
   {
     name: 'The LemonTree Dog',
     price: '8.30',
-    description: 'Our 9 inch quality butchers sausage served in a homemade brioche bun, with a base of sweet mustard, chopped gherkins and peppers. Topped with caramelised red onion chutney and American mustard. Served with chips and homemade coleslaw.',
-    extra: ''    
+    description: 'Our 9 inch quality butchers sausage served in a homemade brioche bun, with a base of sweet mustard, chopped gherkins and peppers. Topped with caramelised red onion chutney and American mustard. Served with chips and homemade coleslaw.'
   },
   {
     name: 'The LemonTree Chilli Dog',
     price: '9.30',
-    description: 'Everything you get with the LemonTree Dog, but topped with our delicious homemade Chilli. Served with chips and homemade coleslaw.',
-    extra: ''    
+    description: 'Everything you get with the LemonTree Dog, but topped with our delicious homemade Chilli. Served with chips and homemade coleslaw.'   
   },
   {
     name: 'The LemonTree Toastie (V)',
     price: '6.65',
-    description: '2 slices of toasted homemade sourdough, crammed with swiss cheese and caramelised onion chutney, served with a dressed salad and homemade coleslaw.',
-    extra: ''    
+    description: '2 slices of toasted homemade sourdough, crammed with swiss cheese and caramelised onion chutney, served with a dressed salad and homemade coleslaw.'
   },
   {
     name: 'The LemonTree Mensieur',
     price: '7.75',
-    description: '2 slices of toasted homemade sourdough with home baked gammon, swiss cheese and wholegrain mustard mayonnaise, served with a dressed salad and homemade coleslaw.',
-    extra: ''    
+    description: '2 slices of toasted homemade sourdough with home baked gammon, swiss cheese and wholegrain mustard mayonnaise, served with a dressed salad and homemade coleslaw.'
   },
   {
     name: 'Classic burger',
     price: '9.95',
-    description: 'A 180g homemade beef patty with a base of sweet mustard, chopped gherkins and peppers, topped with mature Cheddar cheese, baby gem lettuce, red onion and tomato. Served in a homemade brioche bun with chips and coleslaw.',
-    extra: 'Add Bacon for £1.00!'    
+    description: 'A 180g homemade beef patty with a base of sweet mustard, chopped gherkins and peppers, topped with mature Cheddar cheese, baby gem lettuce, red onion and tomato. Served in a homemade brioche bun with chips and coleslaw.<br><strong>Add Bacon for £1.00!</strong>'
   },
   {
     name: 'Gammon, Egg and Chips (GF)',
     price: '8.50',
-    description: 'Home baked gammon, 2 poached free-range eggs and chips.',
-    extra: ''    
+    description: 'Home baked gammon, 2 poached free-range eggs and chips.'
   },
   {
     name: 'Jacket Potato (GF)',
     price: '6.80',
-    description: 'Choose from: Tuna, chicken mayonnaise, homemade chilli, homemade or heinz beans, cheese (cheddar, goats cheese, swiss or brie), prawns in marie rose sauce. Served with dressed salad and homemade coleslaw. (Please note we do not put butter on our potatoes unless requested.)',
-    extra: 'Add an extra topping for £1.00'    
+    description: 'Choose from: Tuna, chicken mayonnaise, homemade chilli, homemade or heinz beans, cheese (cheddar, goats cheese, swiss or brie), prawns in marie rose sauce. Served with dressed salad and homemade coleslaw. (Please note we do not put butter on our potatoes unless requested.)<br><strong>Add an extra topping for £1.00</strong>'
   },
   {
     name: 'Chicken Fajita',
     price: '6.85',
-    description: 'Chicken cooked in a medium spiced fajita mix with peppers, onion and garlic, served in a 12" wrap with a side salad and homemade coleslaw, topped with sour cream.',
-    extra: ''    
+    description: 'Chicken cooked in a medium spiced fajita mix with peppers, onion and garlic, served in a 12" wrap with a side salad and homemade coleslaw, topped with sour cream.'
   },
   {
     name: 'Chilli Nachos',
     price: '6.85',
-    description: 'Nacho tortilla chips covered with our homemade chilli and topped with cheese and sour cream.',
-    extra: ''    
-  },
+    description: 'Nacho tortilla chips covered with our homemade chilli and topped with cheese and sour cream.' 
+  }
 ];
 
 const nibblesItems = [
@@ -160,10 +135,6 @@ const nibblesItems = [
   {
     name: 'Warm Garlic Ciabatta',
     price: '2.80'
-  },
-  {
-    name: '',
-    price: ''
   }
 ];
 
@@ -227,13 +198,7 @@ const sandwichItems = [
     sPrice: '5.95',
     wPrice: '6.25',
     cPrice: '6.50'
-  },
-  {
-    name: '',
-    sPrice: '',
-    wPrice: '',
-    cPrice: ''
-  },
+  }
 ];
 
 // Query selectors
@@ -241,9 +206,10 @@ const breakfast = document.querySelector('.c-menu__menu-section-breakfast');
 const mains = document.querySelector('.c-menu__menu-section-mains');
 const sandwiches = document.querySelector('.c-menu__menu-section-sandwiches');
 const nibbles = document.querySelector('.c-menu__menu-section-nibbles');
+const extras = document.querySelector('.c-menu__extras');
 
 function addExtras(section){
-  section.innerHTML += `<p>Extras</p>`;
+  extras.innerHTML += `<p>Extras</p>`;
 }
 
 
@@ -257,14 +223,6 @@ function generateBreakfasts() {
         <p>${menuItem.description}</p>
       </div>
     `;
-    
-    if(menuItem.extra) {
-      breakfast.innerHTML += `
-      <div class="c-menu__item c-menu__item-extra">
-        <p>${menuItem.extra}</p>
-      </div>
-      `;
-    }
   });
   addExtras(breakfast);
 }
@@ -280,13 +238,6 @@ function generateMains() {
         <p>${menuItem.description}</p>
       </div>
     `;
-    if(menuItem.extra) {
-      mains.innerHTML += `
-        <div class="c-menu__item c-menu__item-extra">
-          <p>${menuItem.extra}</p>
-        </div>
-      `;
-    }
   });
 }
 
